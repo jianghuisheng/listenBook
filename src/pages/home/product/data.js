53404a1b3584ccb742d145afb752bd4e78a6b26d
jianghuisheng <1697965700@qq.com>
2019-04-10 06:58:54 +0800

    first commit

---
+++ src/pages/home/product/data.js
@@ -0,0 +1,38 @@
+var mockData={
+    product_list:[
+        {
+            //商品id
+            product_id:1,
+            //书本封面
+            product_cover:'https://daka666-1253442168.file.myqcloud.com/college_pro_media/goods_cover/1_582c97680e.jpeg',
+            //书本名称
+            product_name:'180天读书计划',
+            //书本价格
+            product_price:'99.00',
+            //书本分成
+            product_welfare:'90%',
+            //书本万粉收益
+            product_fans_profit:'432.00',
+            //书本ARPU
+            product_arpu:'16.30',
+            //是否是新书
+            is_new:0,
+            //是否是热门
+            is_hot:0
+        },
+        {
+           
+            product_id:2,
+            product_cover:'https://daka666-1253442168.file.myqcloud.com/college_pro_media/goods_cover/_2ead77c088.jpeg',
+            product_name:'名人传记·精英阅读计划',
+            product_price:'99.00',
+            product_welfare:'90%',
+            product_fans_profit:'366.00',
+            product_arpu:'13.60',
+            is_new:1,
+            is_hot:1
+        }
+    ]
+}
+
+export default mockData;
